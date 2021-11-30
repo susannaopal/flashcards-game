b93e25898d158a3f90410bba684dcf22eeb0d61d
Susanna Carey <susannaopal@gmail.com>
2021-11-30 16:35:39 -0500

    Write test to instantiate the Deck class and fix the beforeEach

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -12,6 +12,7 @@ describe ('Deck', function() {
   let card3;
 
 beforeEach(function () {
+    deck = new Deck([card1, card2, card3]);
     card1 = new Card(1, "What allows you to define a set of related information using key-value pairs?", ["object", "array", "function"], "object");
     card2 = new Card(2, "What is a comma-separated list of related values?", ["array", "object", "function"],  "array");
     card3 = new Card(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method");
@@ -19,6 +20,10 @@ beforeEach(function () {
 
   it('should be a function', function() {
     expect(Deck).to.be.a('function');
+  });
+
+  it('should instantiate a new instance of Deck', function() {
+    expect(deck).to.be.an.instanceof(Deck);
   });
 
 });

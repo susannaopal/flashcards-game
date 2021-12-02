0cca0c1f2e3a46b28ec6bbbfdf1a10fc3403df60
Susanna Carey <susannaopal@gmail.com>
2021-12-01 20:39:03 -0500

    Write code for beginning game play

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,21 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
   constructor() {}
+    start() {
+    const cardDeck = prototypeQuestions.map((card) => {
+        return card = new Card;
+      });
+    const deck = new Deck(cardDeck);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +25,6 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
-}
+};
 
 module.exports = Game;

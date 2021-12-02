049feb67a04bf8d6f3b5283222a79bb8fa44b1b9
Susanna Carey <susannaopal@gmail.com>
2021-12-02 18:31:18 -0500

    Fix spacing and syntax errors, ignore console log notes

--- src/Round.js
+++ src/Round.js
@@ -1,38 +1,39 @@
 const Turn = require('../src/Turn');
 
-class Round{
-  constructor(deck){
+class Round {
+  constructor(deck) {
     this.deck = deck;
     this.currentCard = deck.deck[0];
     this.turn = 0;
     this.incorrectGuesses = [];
-  };
+  }
 
-  returnCurrentCard(){
+  returnCurrentCard() {
     return this.currentCard;
-  };
+  }
 
-  takeTurn(guess){
+  takeTurn(guess) {
     const turn = new Turn(guess, this.currentCard)
     turn.evaluateGuess()
-    if (turn.evaluateGuess() === false){
+    if (turn.evaluateGuess() === false) {
       this.incorrectGuesses.push(this.currentCard.id)
-    };
+    }
     this.turn += 1;
     this.currentCard = this.deck.deck[this.turn];
     return turn.giveFeedback();
-  };
+  }
 
   calculatePercentCorrect() {
     const guessesTotal = this.turn;
     const totalCorrect = guessesTotal - this.incorrectGuesses.length;
     return (totalCorrect / guessesTotal) * 100;
-  };
+  }
 
   endRound() {
-    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
-    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`;
-  };
-};
+    //console log statement left in per my understanding of the project spec to print message to console
+    console.log(`** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`)
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  }
+}
 
 module.exports = Round;
